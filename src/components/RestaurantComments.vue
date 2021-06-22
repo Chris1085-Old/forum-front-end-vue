8bca0fef721fcb66a4929aedc17504f8ef010ac4
chris <itscfchiang@gmail.com>
2021-06-22 19:53:46 +0800

    feat: Add Restaurant comments

--- src/components/RestaurantComments.vue
+++ src/components/RestaurantComments.vue
@@ -7,9 +7,10 @@
     <div v-for="comment in restaurantComments" :key="comment.id">
       <blockquote class="blockquote mb-0">
         <button
-          v-if="currentUser.isAdmin"
           type="button"
           class="btn btn-danger float-right"
+          v-if="currentUser.isAdmin"
+          @click.stop.prevent="handleDeleteButtonClick(comment.id)"
         >
           Delete
         </button>
@@ -52,6 +53,14 @@ export default {
   data() {
     return {
       currentUser: dummyUser.currentUser
+    }
+  },
+  methods: {
+    handleDeleteButtonClick(commentId) {
+      console.log('handleDeleteButtonClick', commentId)
+      // TODO: 請求 API 伺服器刪除 id 為 commentId 的評論
+      // 觸發父層事件 - $emit( '事件名稱' , 傳遞的資料 )
+      this.$emit('after-delete-comment', commentId)
     }
   }
 }
